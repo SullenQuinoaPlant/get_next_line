e276a478e9a13f4a4f93f9a8d7c9589f9d7d1a3d
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-25 18:42:17 +0100

    i

--- .travis/install_cmocka.sh
+++ .travis/install_cmocka.sh
@@ -4,14 +4,12 @@ git clone https://github.com/clibs/cmocka.git cmocka_download
 mkdir -p cmocka_build
 cd cmocka_build && cmake ../cmocka_download && make
 cd ..
-(
-	echo $LIBS_I
-	echo $LIBS_L
- 	ls cmocka_download/include/cmocka.h
-	ls cmocka_build/src/libcmocka*
- 	mv cmocka_download/include/cmocka.h $LIBS_I
-	mv cmocka_build/src/libcmocka* $LIBS_L
-	rm -rf cmocka_download
-	rm -rf cmocka_build
-)
+echo $LIBS_I
+echo $LIBS_L
+ls cmocka_download/include/cmocka.h
+ls cmocka_build/src/libcmocka*
+mv cmocka_download/include/cmocka.h $LIBS_I
+mv cmocka_build/src/libcmocka* $LIBS_L
+rm -rf cmocka_download
+rm -rf cmocka_build
 echo "exiting cmocka installation script"
