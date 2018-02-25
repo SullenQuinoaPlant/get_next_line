e3b9680ec4075539867a8498f8db451937e31d41
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-26 00:30:26 +0100

    i

--- .travis/install_42FileChecker.sh
+++ .travis/install_42FileChecker.sh
@@ -1,5 +1,3 @@
 #/bin/bash
-git clone https://github.com/jgigault/42FileChecker ~/42FileChecker
 git clone https://github.com/jgigault/42FileChecker.git ~/42FileChecker
-ls ~/42FileChecker -R
-chmod a+x ~/42FileChecker/42FileChecker.sh
+#chmod a+x ~/42FileChecker/42FileChecker.sh
