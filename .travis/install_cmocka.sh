511c9f8065655e6c86dbbb589dcace778d912dc6
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-25 15:33:45 +0100

    i

--- .travis/install_cmocka.sh
+++ .travis/install_cmocka.sh
@@ -2,12 +2,11 @@
 
 git clone https://github.com/clibs/cmocka.git cmocka_download
 mkdir -p cmocka_build
-cd cmocka_build
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_HERE ../cmocka_download
-make
-make install
+cd cmocka_build && cmake ../cmocka_download && make
 cd ..
 (
+	cp cmocka_download/include/cmocka.h $LIBS_I
+	cp cmocka_build/src/libcmocka* $LIBS_L
 	rm -rf cmocka_download -v
 	rm -rf cmocka_build -v
 ) &> /dev/null
