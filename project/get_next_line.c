cc3e26b7af235ac006a8b3e25b99e2dca9bfeeec
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-11 06:57:08 +0200

    i

--- project/get_next_line.c
+++ project/get_next_line.c
@@ -6,123 +6,135 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 03:15:48 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/09/01 03:20:49 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/09/11 06:50:39 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
-static void		edge(char const *h_buff, size_t i, size_t count, t_s_f *s)
+static t_s_fs	*get_set_fd_state(int get_fd, t_s_fs *set)
 {
-	size_t const	ip1 = i + 1;
-	size_t			o_sz;
+	static t_s_fs	anchor = (t_s_fs){-1, 0, 0, 0, 0, 0};
+	t_s_fs			*p;
+	t_s_fs			*prev;
+	char			c;
 
-	if (count == i || (count == BUFF_SIZE && ip1 == count))
-		s->new.o_sz = 0;
-	else
+	if ((p = set))
 	{
-		o_sz = count - ip1;
-		s->new.o_sz = o_sz;
-		ft_memcpy(s->new.over + OVER_SZ - o_sz, h_buff + ip1, o_sz);
+		set->nxt = anchor.nxt;
+		anchor.nxt = set;
 	}
+	if (get_fd < 0)
+		return (p);
+	p = &anchor;
+	prev = &anchor;
+	while (p->fd != get_fd && (p = p->nxt))
+		prev = p;
+	if (p)
+	{
+		prev->nxt = p->nxt;
+		p->nxt = 0;
+	}
+	else if (!read(get_fd, &c, 0) && (p = malloc(sizeof(t_s_fs))))
+		*p = (t_s_fs){get_fd, 0, 0, 0, 0, 0};
+	return (p);
 }
 
-static char		*read_line(char **line, int rank, t_s_f *s)
+static int		edge(char const *h_buff, size_t i, size_t count, int fd)
+{
+	size_t const	ip1 = i + 1;
+	size_t			len;
+	t_s_fs			*new_s;
+
+	if (count > ip1)
+	{
+		len = count - ip1;
+		if ((new_s = malloc(sizeof(t_s_fs))) &&
+			(new_s->buf = malloc(len)))
+		{
+			*new_s = (t_s_fs){fd, new_s->buf, len, new_s->buf, len, 0};
+			ft_memcpy(new_s->buf, h_buff + ip1, len);
+			return (get_set_fd_state(SET_FD, new_s) ? 0 : -1);
+		}
+		else if (new_s)
+			free(new_s);
+	}
+	return (-1);
+}
+
+static int		read_line(char **line, int rank, t_s_fs *fd_s)
 {
 	char	h_buff[BUFF_SIZE];
 	size_t	count;
 	size_t	i;
+	int		r;
 
-	if ((count = read(s->fildes, h_buff, BUFF_SIZE)) != ERROR &&
-			(count || rank || s->old.o_sz))
-	{
-		i = 0;
-		while (i < count && h_buff[i] != EOL)
-			i++;
-		if (i == BUFF_SIZE)
-			read_line(line, rank + 1, s);
-		else if ((*line = malloc(rank * BUFF_SIZE + i + s->old.o_sz + 1)))
-		{
-			edge(h_buff, i, count, s);
-			*line += rank * BUFF_SIZE + i + s->old.o_sz;
-			**line = '\0';
-		}
-		if (*line)
-			while (i--)
-				*--(*line) = h_buff[i];
-	}
-	else if (count == ERROR)
-		*line = 0;
-	return (*line);
-}
-
-static int		known_smallline(t_s_b *b_s, char **line)
-{
-	size_t	i;
-	size_t	ii;
-	size_t	smallline_sz;
-
-	i = OVER_SZ - b_s->o_sz;
-	ii = i;
-	while (i < OVER_SZ)
-	{
-		if (b_s->over[i] == EOL)
-		{
-			smallline_sz = i - ii + 1;
-			if ((*line = malloc(smallline_sz)))
-			{
-				b_s->o_sz -= smallline_sz;
-				(*line)[--smallline_sz] = '\0';
-				while (smallline_sz--)
-					(*line)[smallline_sz] = b_s->over[--i];
-				return (1);
-			}
-			return (-1);
-		}
+	r = -1;
+	if ((count = read(fd_s->fd, h_buff, BUFF_SIZE)) == GNL_ERR)
+		return (r);
+	i = 0;
+	while (i < count && h_buff[i] != EOL)
 		i++;
+	if (i == BUFF_SIZE)
+		r = read_line(line, rank + 1, fd_s);
+	else if ((*line = malloc(fd_s->len + rank * BUFF_SIZE + i + 1)) &&
+		!(r = edge(h_buff, i, count, fd_s->fd)))
+	{
+		*line += fd_s->len + rank * BUFF_SIZE + i;
+		**line = '\0';
 	}
-	return (0);
+	else if (*line)
+		free(*line);
+	if (!r)
+		while (i--)
+			*--(*line) = h_buff[i];
+	return (r);
 }
 
-static t_s_f	*get_fd_states(int fd)
+static int		known_smallline(t_s_fs *fd_s, char **line)
 {
-	static t_s_f	array[A_LOT];
-	char			c;
+	char *const		strt = fd_s->p_b;
+	char *const		lim = strt + fd_s->len;
+	size_t			len;
+	char			*p;
 
-	if (fd >= 0 && fd < A_LOT && !read(fd, &c, 0))
-	{
-		array[fd].fildes = fd;
-		return (array + fd);
-	}
+	p = strt;
+	while (p < lim)
+		if (*p++ == EOL)
+		{
+			len = p - strt;
+			if (len && !(*line = malloc(len--)))
+				return (-1);
+			(*line)[len] = '\0';
+			ft_memcpy(*line, strt, len);
+			fd_s->p_b = p;
+			fd_s->len -= len;
+			return (1);
+		}
 	return (0);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	int		ret;
-	t_s_f	*fd_state;
-	size_t	i;
+	t_s_fs	*fd_s;
 
-	ret = -1;
-	if (!line)
-		return (ret);
-	*line = UNALLOCATED;
-	if ((fd_state = get_fd_states(fd)) &&
-			!(ret = known_smallline(&fd_state->old, line)) &&
-			(ret = read_line(line, 0, fd_state) ? 0 : -1) != -1 &&
-			*line != UNALLOCATED)
+	if (!line || !(fd_s = get_set_fd_state(fd, GET_FD)))
+		return (-1);
+	else if (!(ret = known_smallline(fd_s, line)) &&
+			!(ret = read_line(line, 0, fd_s)))
 	{
-		i = 0;
-		while (i++ < fd_state->old.o_sz)
-			*--(*line) = fd_state->old.over[OVER_SZ - i];
-		fd_state->old = fd_state->new;
-		ret = 1;
+		ft_memcpy((*line -= fd_s->len), fd_s->p_b, fd_s->len);
+		fd_s->len = 0;
 	}
-	else if (*line == UNALLOCATED)
-		*line = 0;
+	if (!fd_s->len)
+	{
+		ft_cleanfree(fd_s->buf, fd_s->b_sz);
+		ft_cleanfree(fd_s, sizeof(t_s_fs));
+	}
+	else
+		get_set_fd_state(SET_FD, fd_s);
+	if (ret == -1)
+		*line = malloc(0);
 	return (ret);
 }
--- project/get_next_line.h
+++ project/get_next_line.h
@@ -6,36 +6,34 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 03:15:44 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/09/06 04:50:01 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/09/11 06:47:56 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
 # include "libft.h"
 
-# define ERROR ~((size_t)0)
-
 # define BUFF_SIZE 1000
-# define OVER_SZ BUFF_SIZE - 1
 # define EOL '\n'
-# define BIGBIGADR ~((size_t)0)
-# define UNALLOCATED (char*)0 + BIGBIGADR
+# define GNL_ERR (size_t)-1
 
-typedef struct	s_buff_state {
-	size_t	o_sz;
-	char	over[OVER_SZ];
-}				t_s_b;
+# define GET_FD 0
+# define SET_FD -1
 
-typedef struct	s_fildes_state {
-	int			fildes;
-	t_s_b		old;
-	t_s_b		new;
-}				t_s_f;
+typedef struct s_fildes_state	t_s_fs;
+struct							s_fildes_state {
+	int			fd;
+	char		*buf;
+	size_t		b_sz;
+	char		*p_b;
+	size_t		len;
+	t_s_fs		*nxt;
+};
 
-# define A_LOT 500
-
-int				get_next_line(const int fd, char **line);
+int								get_next_line(const int fd, char **line);
 
 #endif
