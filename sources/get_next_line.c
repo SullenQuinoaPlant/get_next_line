645941e4e7ee19ddb44b4dc60852886d0b639462
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-12 19:16:39 +0200

    i

--- sources/get_next_line.c
+++ sources/get_next_line.c
@@ -27,9 +27,8 @@ static t_s_fs	*get_set_fd_state(int get_fd, t_s_fs *set)
 	else if (get_fd >=  0)
 	{
 		p = &anchor;
-		prev = &anchor;
-		while (p->fd != get_fd && (p = p->nxt))
-			prev = p;
+		while (p && p->fd != get_fd && (prev = p))
+			p = p->nxt;
 		if (p)
 			prev->nxt = p->nxt;
 		else if (!read(get_fd, &c, 0) && (p = malloc(sizeof(t_s_fs))))
@@ -44,21 +43,20 @@ static int		edge(char const *h_buff, size_t i, size_t count, int fd)
 	size_t			len;
 	t_s_fs			*new_s;
 
-	if (count > ip1)
+	if (count <= ip1)
+		return (0);
+	len = count - ip1;
+	if ((new_s = malloc(sizeof(t_s_fs))) &&
+		(new_s->buf = malloc(len)))
 	{
-		len = count - ip1;
-		if (!(new_s = malloc(sizeof(t_s_fs))) ||
-			!(new_s->buf = malloc(len)))
-		{
-			if (new_s)
-				free(new_s);
-			return (-1);
-		}
 		*new_s = (t_s_fs){fd, new_s->buf, len, new_s->buf, len, 0};
 		ft_memcpy(new_s->buf, h_buff + ip1, len);
 		get_set_fd_state(SET_FD, new_s);
+		return (0);
 	}
-	return (0);
+	else
+		free(new_s);
+	return (-1);
 }
 
 static int		read_line(char **line, int rank, t_s_fs *fd_s)
