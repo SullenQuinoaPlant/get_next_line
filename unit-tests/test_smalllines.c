300f244d9193f71f8f459977011c50987b09b314
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-15 16:32:46 +0200

    i

---
+++ unit-tests/test_smalllines.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "get_next_line.c"
+
+int		main()
+{
+	char *line;
+	int fd;
+	int fd2;
+    
+	fd = open("testsmallline.txt", O_RDONLY);
+	fd2 = open("res.txt", O_CREAT | O_RDWR | O_TRUNC, 0755);
+
+	while (get_next_line(fd, &line) == 1)
+	{
+	    write(fd2, line, strlen(line));
+		if (line)
+			free(line);
+	    write(fd2, "\n", 1);
+	}
+	if (line)
+	{
+		write(fd2, line, strlen(line));
+		free(line);
+	}
+	close(fd);
+	close(fd2);
+	return (0);
+}
